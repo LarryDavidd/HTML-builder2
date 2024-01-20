0fffc8e6b6b63be20f6a8beb35a8583646e8b844
Airat2000 <gerasimovevgeni16082003@gmail.com>
2024-01-20 14:38:48 +0400

    feat: add first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+stream.on('data', (chunk) => stdout.write(chunk));
+stream.on('error', (e) => console.log(e.message));
